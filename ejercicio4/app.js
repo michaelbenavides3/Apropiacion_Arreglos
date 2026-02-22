85cd13ee78d83149b3712874d17cc8609dd4dee9
michaelbenavides3 <michaelfb803@gmail.com>
2026-02-22 14:49:33 -0500

    agregege ejercicio4, se creo un objeto llamado celular, y se le asignaron las propiedas, y se impirmieron de manera individaul por consola

---
+++ ejercicio4/app.js
@@ -0,0 +1,17 @@
+//Características de un celular: Cree un objeto llamado celular con propiedades:
+//marca, modelo, año y estado. Muestre cada propiedad en consola de manera
+//individual.
+
+//creamis un objeto llamado celuarla y le asignamos las propiedades
+const celular = {
+    marca: "NOKIA",
+    modelo: 1100,
+    año: 2000,
+    estado: "resistente"
+
+}
+//asi accedemos de manera individual a cada propiedad
+console.log( "marca: ", celular.marca);
+console.log( "modelo: ",celular.modelo);
+console.log( "el año: ", celular.año);
+console.log( "su estado: ", celular.estado);
